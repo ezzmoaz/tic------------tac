52f3ba3f15e0093453ccc93e747f2917ce06947e
AMRALAA10 <amr.alaa20191404@gmail.com>
2016-03-14 17:07:38 +0200

    trying main func

--- src/tic/tac/TicTac.java
+++ src/tic/tac/TicTac.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tic.tac;
 
-/**
- *
- * @author Ezzmoaz
- */
 public class TicTac {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Player player1 = new Player(1);
+        Player player2 = new Player(0);
+        
+        Game geme = new game();
+        
     }
     
 }
