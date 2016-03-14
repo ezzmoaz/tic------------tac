dd0e086eaf4328f08021295d444907f6a696c994
Ezz Moaz <Ezzmoaz@Ezzs-MacBook-Pro.local>
2016-03-14 17:04:58 +0200

    end of func game

--- src/tic/tac/Game.java
+++ src/tic/tac/Game.java
@@ -10,4 +10,49 @@ package tic.tac;
  * @author Ezzmoaz
  */
 public class Game {
-}
+    
+    int gameBoard[][] = new int[6][7];  
+    
+    public boolean CheckWhoWins(int col,int row,Player player){
+        
+        int x=player.getPlayerValue();
+        
+            
+           if(this.gameBoard[row+1][col]==x){
+               if(this.gameBoard[row+2][col]==x)return true;
+           }
+           if(this.gameBoard[row-1][col]==x){
+               if(this.gameBoard[row-2][col]==x)return true;
+           }
+           if(this.gameBoard[row][col+1]==x){
+               if(this.gameBoard[row][col+2]==x)return true;
+           }
+           if(this.gameBoard[row][col-1]==x){
+               if(this.gameBoard[row][col-2]==x)return true;
+           }
+           if(this.gameBoard[row+1][col+1]==x){
+               if(this.gameBoard[row+2][col+2]==x)return true;
+           }
+           if(this.gameBoard[row-1][col-1]==x){
+               if(this.gameBoard[row-2][col-2]==x)return true;
+           }
+           if(this.gameBoard[row+1][col-1]==x){
+               if(this.gameBoard[row+2][col-2]==x)return true;
+           }
+           if(this.gameBoard[row-1][col+1]==x){
+               if(this.gameBoard[row-2][col+2]==x)return true;
+           }
+           
+           return false;
+            
+            
+            
+            
+
+           
+          
+           
+        }
+        
+        
+    }
